4dcbdc1b5c514307a2e823c49b75a7cc70a70b54
sobonn <sobonn.8921@gmail.com>
2021-02-17 21:07:05 +0900

    [Clean] 4 spaces to tab

--- src/index.js
+++ src/index.js
@@ -129,32 +129,32 @@
 
 	/* 演習1 ここから */
 
-    function sub(){}
-    function add(){}
+	function sub(){}
+	function add(){}
 
 	/* 演習1 ここまで */
 
 	/* TODO: 演習2 以降は下記に自由に記入 */
-    function add(){
-        eqlNum.value = parseFloat(midNum[0].value) + parseFloat(midNum[1].value);
-    }
+	function add(){
+		eqlNum.value = parseFloat(midNum[0].value) + parseFloat(midNum[1].value);
+	}
 
-    function sub(){
-        eqlNum.value = parseFloat(midNum[0].value) - parseFloat(midNum[1].value);
-    }
+	function sub(){
+		eqlNum.value = parseFloat(midNum[0].value) - parseFloat(midNum[1].value);
+	}
 
 
-    function div(){
-        if(parseFloat(midNum[1].value) == 0){
-            eqlNum.value = "ERROR";
-        }else{
-            eqlNum.value = parseFloat(midNum[0].value) / parseFloat(midNum[1].value);
-        }
-    }
-    
-    function mul(){
-        eqlNum.value = parseFloat(midNum[0].value) * parseFloat(midNum[1].value);
-    }
+	function div(){
+		if(parseFloat(midNum[1].value) == 0){
+			eqlNum.value = "ERROR";
+		}else{
+			eqlNum.value = parseFloat(midNum[0].value) / parseFloat(midNum[1].value);
+		}
+	}
+	
+	function mul(){
+		eqlNum.value = parseFloat(midNum[0].value) * parseFloat(midNum[1].value);
+	}
 
 	/* 演習の編集範囲 おわり */
 
